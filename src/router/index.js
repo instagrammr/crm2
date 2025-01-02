8d99e3215d1eea3f500d9d95653b794d25dfb9e4
unknown <hexpdevfltr@gmail.com>
2025-01-02 14:27:28 +0530

    Fix: Update Vercel configuration for SPA routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,12 @@
+
+
 import { createRouter, createWebHistory } from '@ionic/vue-router';
-import HomePage from '../views/Home.vue';
-import AdminHome from '../views/admin/Adasbord.vue';
-import login from "../views/login.vue";
+
+
+// Import your views
+ import HomePage from '../views/Home.vue';
+ import AdminHome from '../views/admin/Adasbord.vue';
+ import login from "../views/login.vue";
 import employees from "../views/admin/employees.vue";
 import attendance from "../views/admin/attendance.vue";
 import registered from "../views/admin/registered-leads.vue";
@@ -14,12 +19,13 @@ import addemployees from "../views/admin/addemployees.vue";
 import addlead from "../views/addlead/addlead.vue";
 import attendanceAdd from "../views/addlead/attendanceAdd.vue";
 import viewlead from "../views/addlead/viewlead.vue";
-import leaddashbord from '../views/addlead/leadDashbord.vue';
+import leaddashbord from '../views/addlead/leadDashbord.vue'; 
 import leaverequest4 from "../views/addlead/leaverequest4.vue";
-import cldashbord from '../views/Counselor/dashbord.vue';
-import clnewlead from '../views/Counselor/newlead.vue';
-import leaverequest from '../views/Counselor/leaverequest.vue';
-import leaverequest1 from '../views/admin/leaverequest1.vue';
+  
+import cldashbord from '../views/Counselor/dashbord.vue'; 
+import clnewlead  from '../views/Counselor/newlead.vue';
+import leaverequest  from '../views/Counselor/leaverequest.vue';
+import leaverequest1  from '../views/admin/leaverequest1.vue';
 import clfollowupleads from '../views/Counselor/followupleads.vue';
 import registerdleads from '../views/Counselor/registerdleads.vue';
 import totallleads from '../views/Counselor/totallleads.vue';
@@ -37,7 +43,7 @@ import ADashbord from '../views/application/Dashboard.vue';
 import DropedApplications from '../views/application/DropedApplications.vue';
 import FollowupApplicatio from '../views/application/FollowupApplicatio.vue';
 import NewApplications from '../views/application/NewApplications.vue';
-import registeredApplications from '../views/application/registeredApplications.vue';
+import registeredApplications  from '../views/application/registeredApplications.vue';
 import TotalApplications from '../views/application/TotalApplications.vue';
 import attendance1 from '../views/Documentation/attandence1.vue';
 import leaverequest2 from '../views/Documentation/leaverequest2.vue';
@@ -60,328 +66,400 @@ import leadStatusCouns from '../views/Counselor/leadStatusCouns.vue';
 import leadStatusDoc from '../views/Documentation/leadStatusDoc.vue';
 import leadStatusApp from '../views/application/leadStatusApp.vue';
 
+
+
+// import leaverequest from "../views/admin/leave-request.vue"; //leavereques
+
+
 const routes = [
+ 
+
   {
     path: '/',
     name: 'login',
-    component: login
+    component: login,
+    // beforeEnter: checkAuth
   },
   {
     path: '/home',
     name: 'Home',
-    component: HomePage
+    component: HomePage,
+    // beforeEnter: checkAuth
   },
-  // Admin Routes
   {
     path: '/admin/dasbord/:id',
     name: 'Adasbord',
-    component: AdminHome
+    component: AdminHome,
+    // beforeEnter: checkAuth
   },
   {
     path: '/admin/employees',
     name: 'employees',
-    component: employees
+    component: employees,
+    // beforeEnter: checkAuth
   },
   {
     path: '/admin/attendance',
     name: 'attendance',
-    component: attendance
+    component: attendance,
+    // beforeEnter: checkAuth
   },
   {
     path: '/admin/registered/:id',
     name: 'registered',
-    component: registered
+    component: registered,
+    // beforeEnter: checkAuth
   },
   {
     path: '/admin/followup-leads/:id',
     name: 'followup',
-    component: followup
+    component: followup,
+    // beforeEnter: checkAuth
   },
   {
     path: '/admin/cancelled-leads/:id',
     name: 'cancelled-leads',
-    component: cancelledleads
+    component: cancelledleads,
+    // beforeEnter: checkAuth
   },
+
   {
     path: '/admin/Totalleads',
     name: 'Totalleads',
-    component: Totalleads
+    component: Totalleads,
+    // beforeEnter: checkAuth
   },
   {
     path: '/admin/leave-request1/:id',
     name: 'leaverequest1',
-    component: leaverequest1
+    component: leaverequest1,
+    // beforeEnter: checkAuth
   },
   {
     path: '/admin/leadStatus',
     name: 'leadStatus',
-    component: leadStatus
+    component: leadStatus,
+    // beforeEnter: checkAuth
   },
   {
     path: '/admin/StudentStatus',
     name: 'StudentStatus',
-    component: StudentStatus
+    component: StudentStatus,
+    // beforeEnter: checkAuth
   },
   {
     path: '/admin/birthday',
     name: 'Birthday',
-    component: Birthday
+    component: Birthday,
+    // beforeEnter: checkAuth
   },
   {
     path: '/admin/addemployees',
     name: 'addemployees',
-    component: addemployees
+    component: addemployees,
+    // beforeEnter: checkAuth
   },
   {
     path: '/admin/addleads/:id',
     name: 'addleads',
-    component: addleads
+    component: addleads,
+    // beforeEnter: checkAuth
   },
-
-  // Add Lead Routes
   {
     path: '/addlead/leaddashbord/:id',
     name: 'leaddashbord',
-    component: leaddashbord
-  },
+    component: leaddashbord,
+    // beforeEnter: checkAuth
+  }, 
   {
     path: '/addlead/addlead/:id',
     name: 'addlead',
-    component: addlead
+    component: addlead,
+    // beforeEnter: checkAuth
   },
   {
-    path: '/addlead/attendanceAdd/:id',
-    name: 'attendanceAdd',
-    component: attendanceAdd
+     path:'/addlead/attendanceAdd/:id',
+     name:'attendanceAdd',
+     component:attendanceAdd,
   },
   {
-    path: '/addlead/leaverequest4/:id',
-    name: 'leaverequest4',
-    component: leaverequest4
-  },
+    path:'/addlead/leaverequest4/:id',
+    name:'leaverequest4',
+    component:leaverequest4,
+ },
+
   {
     path: '/addlead/viewlead/:id',
     name: 'viewlead',
-    component: viewlead
+    component: viewlead,
+    // beforeEnter: checkAuth
   },
   {
     path: '/addlead/birthday',
     name: 'BirthdayAdd',
-    component: BirthdayAdd
+    component: BirthdayAdd,
+    // beforeEnter: checkAuth
   },
   {
     path: '/addlead/StudentStatusAdd',
     name: 'studentStatusAdd',
-    component: studentStatusAdd
+    component: studentStatusAdd,
+    // beforeEnter: checkAuth
   },
-
-  // Counselor Routes
   {
     path: '/Counselor/Dashbord/:id',
     name: 'cldashbord',
-    component: cldashbord
+    component:cldashbord,
+    // beforeEnter: checkAuth
   },
   {
     path: '/Counselor/notification/:id',
     name: 'notificationCouns',
-    component: Notification
+    component: Notification,
+    // beforeEnter: checkAuth
   },
   {
     path: '/Counselor/newleads/:id',
     name: 'clnewlead',
-    component: clnewlead
+    component:clnewlead,
+    // beforeEnter: checkAuth
   },
   {
     path: '/Counselor/followupleads/:id',
     name: 'clfollowupleads',
-    component: clfollowupleads
+    component:clfollowupleads,
+    // beforeEnter: checkAuth
   },
+
   {
     path: '/Counselor/registerdleads/:id',
     name: 'registerdleads',
-    component: registerdleads
+    component:registerdleads,
+    // beforeEnter: checkAuth
   },
   {
-    path: '/Counselor/totallleads/:id',
+    path: `/Counselor/totallleads/:id`,
     name: 'totallleads',
-    component: totallleads
+    component:totallleads,
+    // beforeEnter: checkAuth
   },
+
   {
     path: '/Counselor/leadsform',
     name: 'leadform',
-    component: leadform
+    component:leadform,
+    // beforeEnter: checkAuth
   },
+
   {
     path: '/Counselor/cancelled/:id',
     name: 'cancelled',
-    component: cancelled
+    component:cancelled,
+    // beforeEnter: checkAuth
   },
   {
     path: '/Counselor/leave-request/:id',
     name: 'leaverequest',
-    component: leaverequest
+    component:leaverequest,
+    // beforeEnter: checkAuth
   },
   {
     path: '/Counselor/BirthdayCouns/:id',
     name: 'BirthdayCouns',
-    component: BirthdayCouns
+    component: BirthdayCouns,
+    // beforeEnter: checkAuth
   },
   {
     path: '/Counselor/studentStatusCouns/:id',
     name: 'studentStatusCouns',
-    component: studentStatusCouns
+    component:studentStatusCouns,
+    // beforeEnter: checkAuth
   },
   {
     path: '/Counselor/leadStatusCouns/:id',
     name: 'leadStatusCouns',
-    component: leadStatusCouns
+    component: leadStatusCouns,
+    // beforeEnter: checkAuth
   },
   {
     path: '/Counselor/attandence/:id',
     name: 'attandence',
-    component: attandence
-  },
-
-  // Documentation Routes
-  {
-    path: '/documentation/dashboard/:id',
-    name: 'Dashbord',
-    component: Dashbord
-  },
-  {
-    path: '/documentation/NotificationDoc/:id',
-    name: 'NotificationDoc',
-    component: NotificationDoc
-  },
-  {
-    path: '/documentation/new-documents/:id',
-    name: 'newdocuments',
-    component: newdocuments
-  },
-  {
-    path: '/documentation/followup-documents/:id',
-    name: 'followupdoc',
-    component: followupdoc
-  },
-  {
-    path: '/documentation/total-documents/:id',
-    name: 'TotalDocuments',
-    component: TotalDocuments
-  },
-  {
-    path: '/documentation/dropped-documents/:id',
-    name: 'DropedDocuments',
-    component: DropedDocuments
-  },
-  {
-    path: '/documentation/registered-applications/:id',
-    name: 'registereddocument',
-    component: registereddocument
-  },
-  {
-    path: '/documentation/attandence/:id',
-    name: 'attendance1',
-    component: attendance1
-  },
-  {
-    path: '/documentation/leave-request2/:id',
-    name: 'leaverequest2',
-    component: leaverequest2
-  },
-  {
-    path: '/documentation/BirthdayDoc/:id',
-    name: 'BirthdayDoc',
-    component: BirthdayDoc
-  },
-  {
-    path: '/documentation/leadStatusDoc/:id',
-    name: 'leadStatusDoc',
-    component: leadStatusDoc
-  },
-  {
-    path: '/documentation/studentStatusDoc/:id',
-    name: 'studentStatusDoc',
-    component: studentStatusDoc
-  },
-
-  // Application Routes
-  {
-    path: '/Application/Dashbord/:id',
-    name: 'ADashbord',
-    component: ADashbord
-  },
-  {
-    path: '/Application/NotificationApp/:id',
-    name: 'NotificationApp',
-    component: NotificationApp
-  },
-  {
-    path: '/Application/DropedApplications/:id',
-    name: 'DropedApplications',
-    component: DropedApplications
-  },
-  {
-    path: '/Application/FollowupApplication/:id',
-    name: 'FollowupApplicatio',
-    component: FollowupApplicatio
-  },
-  {
-    path: '/Application/NewApplications/:id',
-    name: 'NewApplications',
-    component: NewApplications
-  },
-  {
-    path: '/Application/registeredApplications/:id',
-    name: 'registeredApplications',
-    component: registeredApplications
-  },
-  {
-    path: '/Application/TotalApplications/:id',
-    name: 'TotalApplications',
-    component: TotalApplications
-  },
-  {
-    path: '/Application/attandence2/:id',
-    name: 'attendance2',
-    component: attendance2
-  },
-  {
-    path: '/Application/leave-request3/:id',
-    name: 'leaverequest3',
-    component: leaverequest3
-  },
-  {
-    path: '/Application/BirthdayApp/:id',
-    name: 'BirthdayApp',
-    component: BirthdayApp
-  },
-  {
-    path: '/Application/leadStatusApp/:id',
-    name: 'leadStatusApp',
-    component: leadStatusApp
-  },
-  {
-    path: '/Application/student_status/:id',
-    name: 'studentStatusApp',
-    component: studentStatusApp
-  },
-  
-  // Catch-all 404 route
-  {
-    path: '/:pathMatch(.*)*',
-    redirect: '/'
+    component:attandence,
+    // beforeEnter: checkAuth
   }
+
+///Documentation
+,
+{
+  path: '/documentation/dashboard/:id',
+  name: 'Dashbord',
+  component:Dashbord,
+  // beforeEnter: checkAuth
+},
+{
+  path: '/documentation/NotificationDoc/:id',
+  name: 'NotificationDoc',
+  component: NotificationDoc,
+  // beforeEnter: checkAuth
+},
+{
+  path: '/documentation/new-documents/:id',
+  name: 'newdocuments',
+  component:newdocuments,
+  // beforeEnter: checkAuth
+},
+{
+  path: '/documentation/followup-documents/:id',
+  name: 'followupdoc',
+  component:followupdoc,
+  // beforeEnter: checkAuth
+},
+{
+  path: '/documentation/total-documents/:id',
+  name: 'TotalDocuments',
+  component:TotalDocuments,
+  // beforeEnter: checkAuth
+},
+{
+  path: '/documentation/dropped-documents/:id',
+  name: 'DropedDocuments',
+  component:DropedDocuments,
+  // beforeEnter: checkAuth
+}
+,
+{
+  path: '/documentation/registered-applications/:id',
+  name: 'registereddocument',
+  component:registereddocument,
+  // beforeEnter: checkAuth
+},
+{
+  path: '/documentation/attandence/:id',
+  name: 'attendance1',
+  component:attendance1,
+  // beforeEnter: checkAuth
+},
+{
+  path: '/documentation/leave-request2/:id',
+  name: 'leaverequest2',
+  component:leaverequest2,
+  // beforeEnter: checkAuth
+},
+{
+  path: '/documentation/BirthdayDoc/:id',
+  name: 'BirthdayDoc',
+  component: BirthdayDoc,
+  // beforeEnter: checkAuth
+},
+{
+  path: '/documentation/leadStatusDoc/:id',
+  name: 'leadStatusDoc',
+  component: leadStatusDoc,
+  // beforeEnter: checkAuth
+},
+{
+  path: '/documentation/studentStatusDoc/:id',
+  name: 'studentStatusDoc',
+  component:studentStatusDoc,
+  // beforeEnter: checkAuth
+},
+
+///application
+{
+  path: '/Application/Dashbord/:id',
+  name: 'ADashbord',
+  component:ADashbord,
+  // beforeEnter: checkAuth
+},
+{
+  path: '/Application/NotificationApp/:id',
+  name: 'NotificationApp',
+  component: NotificationApp,
+  // beforeEnter: checkAuth
+},
+{
+  path: '/Application/DropedApplications/:id',
+  name: 'DropedApplications',
+  component:DropedApplications,
+  // beforeEnter: checkAuth
+}
+
+,{
+  path: '/Application/FollowupApplication/:id',
+  name: 'FollowupApplicatio',
+  component:FollowupApplicatio,
+  // beforeEnter: checkAuth
+}
+
+,{
+  path: '/Application/NewApplications/:id',
+  name: 'NewApplications',
+  component:NewApplications,
+  // beforeEnter: checkAuth
+}
+,{
+  path: '/Application/registeredApplications/:id',
+  name: 'registeredApplications',
+  component:registeredApplications,
+  // beforeEnter: checkAuth
+}
+,{
+  path: '/Application/TotalApplications/:id',
+  name: 'TotalApplications',
+  component:TotalApplications,
+  // beforeEnter: checkAuth
+},
+{
+  path: '/Application/attandence2/:id',
+  name: 'attendance2',
+  component:attendance2,
+  // beforeEnter: checkAuth
+},
+{
+  path: '/Application/leave-request3/:id',
+  name: 'leaverequest3',
+  component:leaverequest3,
+  // beforeEnter: checkAuth
+},
+{
+  path: '/Application/BirthdayApp/:id',
+  name: 'BirthdayApp',
+  component: BirthdayApp,
+  // beforeEnter: checkAuth
+},
+{
+  path: '/Application/leadStatusApp/:id',
+  name: 'leadStatusApp',
+  component: leadStatusApp,
+  // beforeEnter: checkAuth
+},
+{
+  path: '/Application/student_status/:id',
+  name: 'studentStatusApp',
+  component:studentStatusApp,
+  // beforeEnter: checkAuth
+},
+
+//viewlead
+  // {
+  //   path: '/admin/leaverequest',
+  //   name: 'leaverequest',
+  //   component: leaverequest,
+  //   // beforeEnter: checkAuth
+  // },
+ 
+
 ];
 
+// Create the router instance
 const router = createRouter({
-  history: createWebHistory('/'),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
 
-// Navigation guard (optional)
-router.beforeEach((to, from, next) => {
-  // Add any navigation guards here if needed
-  next();
-});
+
+
+
 
 export default router;
+
